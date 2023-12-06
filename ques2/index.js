0e469380fba583a65864a2f1d2c388062d502f43
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-06 15:45:36 +0530

    quest2 - practice

--- ques2/index.js
+++ ques2/index.js
@@ -3,6 +3,27 @@
 Create a web app where I can input a text. Now create two buttons + and -. On clicking + increase the fontSize by 2px and vice versa.
 */
 
+const textInput = document.getElementById("text-input");
+const increaseBtn = document.getElementById("increase-btn");
+const decreaseBtn = document.getElementById("decrease-btn");
+const result = document.getElementById("result");
+
+let defaultFontSize = 16;
+
+function updateSize(){
+    textInput.style.fontSize = defaultFontSize;
+    result.textContent = `Size:${defaultFontSize}px`
+}
+
+increaseBtn.addEventListener("click",()=>{
+    defaultFontSize += 2;
+    updateSize();
+})
+
+decreaseBtn.addEventListener("click",()=>{
+    defaultFontSize -= 2;
+    updateSize();
+})
 
 
-
+updateSize();
