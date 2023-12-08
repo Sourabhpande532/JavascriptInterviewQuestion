1a7f598c940b41592f0146b51bbd7321e96e022c
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-08 16:35:25 +0530

    solve - data type related question

--- Js-Questions/app.js
+++ Js-Questions/app.js
@@ -1,3 +1,22 @@
-console.log(typeof []); 
-console.log(Boolean([]));
-console.log(!![]); 
+
+console.log(typeof []); //Object 
+// console.log(Boolean([])); //True->Why? i.e Truthy value 
+// console.log(!![]); //True->Why? i.e Truthy value
+// console.log(Boolean(""));//false-> i.e falsy value false, 0, '' (empty string), null, undefined, and NaN.
+// console.log(typeof Boolean([])); //boolean -> it treated as string which is true i.e boolean 
+
+// console.log(true); //true
+// console.log(typeof true); //boolean
+// console.log(typeof true === "boolean"); // true
+
+// console.log(false); // false
+// console.log(typeof false); // boolean
+// console.log(typeof false === "boolean"); // true
+
+// console.log(typeof ""); // string
+// console.log(Boolean("")) // false : data types
+// console.log(Boolean(0)) // false
+// console.log(typeof Boolean("")); // boolean
+
+console.log(Boolean("rahim")); // true 
+// true ? console.log(true) : console.log(false);
