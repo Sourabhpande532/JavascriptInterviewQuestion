a59dfeada7064ac2ed64ccc3ed96e09e0ea22235
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-12 16:16:58 +0530

    understand basic consept

--- Js-Questions/app.js
+++ Js-Questions/app.js
@@ -141,7 +141,7 @@ besides Here we are assigning arrayX to arrayY, where memory location is now sam
 /* EXPLANATION:the values of a and b have been swapped. The variable c is used as a temporary storage space to hold the value of a during the swap process.  */
 
 /* ============================================================================== */
-/* IMPORTANT QUESTION!! */
+/* IMPORTANT QUESTION!! TOP 10 */
 /* 
 1)Question: what is typeof [];
 
@@ -157,15 +157,16 @@ Answer: When you use typeof on arguments, it returns "object." The arguments obj
 e.g 
 function exampleArgument(){
 console.log(typeof arguments);// Object
+console.log(arguments instancesOf Array ) //false;
+console.log("Number of arguments:", arguments.length);
 for(let i=0;i<arguments.length;i++){
     console.log("Index at " + i + " is " + arguments[i]);
 }
 }
 exampleArgument('apple','banana','berry')
 
-Question:Difference between array and object in javascript? or Array Vs Object
+3)Question:Difference between array and object in javascript? or Array Vs Object
 Answer: As everything in js are objects,we know see e.g below to more 
-*/
 
 let arr = []
 let obj = {}
@@ -174,3 +175,92 @@ if(typeof arr == typeof obj){
 }else{
     console.log("false");
 }
+if(arr instanceof Object && arr instanceof Array){
+    console.log(true);
+}
+
+3) Question: what is value of typeof null?
+Answer: "Object" Why it's js bug that's why 
+console.log(typeof null == "object");
+
+4)Question: what is console.log(true+false)
+Answer: 1 because true value consider as 1 and false 0 therefour console.log(1+0); // 1
+
+5)Question : what is 2+true;
+Answer: 3 
+Explaination: console.log(2+true);
+The '+' operator performs type coercion in this case to enable the operation, effectively casting the boolean 'true' to its numeric equivalent, which is '1'.
+
+5)Question : what is "2"+true;
+Answer: 2true
+Explaination:
+The + operator is acting as a concatenation operator in this context due to the presence of a string operand.
+In JavaScript, when you use the + operator with a string and another type, it performs concatenation and converts the non-string operand to a string. This is known as type coercion.
+
+6)Question:  What is the value of -'34'+14
+Answer: -20
+Explaination: console.log(-"34"+14);
+The - '34' part is performing a unary negation operation on the string '34'.
+JavaScript converts the string '34' to the number 34 due to the unary negation.
+
+6)Question:  What is the value of +"hello"
+Answer: NaN
+Explaination: console.log(+"hello"); 
+same process as we performed earlier it turn string to number but Js failed to identified this time that's why NaN
+*/
+
+// 7)Question: If you have var y = 1, x = y = typeof x; What is the value of x?
+// Answer: undefined
+// Explaination: typeof x: The typeof operator is applied to x. At this point, x is declared but not yet initialized, so its type is 'undefined'.
+// ----------------------------------------------------------------------------]
+
+// 8)Question: for var a = (2, 38, 50); what is the value of a?
+// Answer: 50. The comma operator evaluates each of its operands (from left to right) and returns the value of the last operand. ref: MDN
+// Code eg: let a = (2,3,4);
+// console.log(a);
+// ---------------------------------------------------------------------------]
+
+// 9)Question: for var a = ((34,43),50-5) * 2 what is the value of a?
+// Answer: 90
+// Explaination:The comma operator is used to evaluate multiple expressions but only returns the value of the last one.The result of the comma operator, 4, is then multiplied by 2, resulting in the final value of a being 8.
+// code ex:
+// let a = (3,34-4) * 2;
+// console.log(a); //60
+
+// let x = ((34,43),50-5) * 2;
+// console.log(x);//90
+
+// let y = (4,(34-3),4)*3;
+// console.log(y);
+
+// ---------------------------------------------------------------------------]
+// 10)Question: What is the value of !'bang'
+// Answer: false. ! is NOT.
+// ---------------------------------------------------------------------------]
+
+
+
+
+/* BASIC QUESTION ON JS  */
+// Question: What is the value of parseFloat('12.3.4')
+// Answer: 12.3
+// console.log(parseFloat("3.47 is more power ful 2.3"));
+// ---------------------------------------------------------------------------]
+// Question: What is the value of Math.max([2,3,4,5]);
+// Answer: NaN
+// Explaination: The Math.max function in JavaScript is designed to accept a list of numeric arguments and return the largest value among them. so in order to perform use below one
+//CODE:
+// let numericValue = [3434,4,343,5500,34];
+// let maxVal = Math.min(...numericValue);
+// console.log(maxVal);
+
+// let x = [34,343,43,4];
+// console.log(Math.max(...x));
+// ---------------------------------------------------------------------------]
+// Question: typeof(NaN)
+// Anwser:"number
+// console.log(typeof NaN);
+// console.log(typeof undefined);
+
+// ---------------------------------------------------------------------------]
+console.log(null == undefined);
